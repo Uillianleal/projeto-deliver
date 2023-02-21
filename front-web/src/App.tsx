3f6a429dc4166b420680404d0065038146d5617f
Uillian Leal <willianleal52@hotmail.com>
2023-02-21 12:26:28 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
